c3b3fd09990941fd708c1deedb9762d883991deb
SOLRAK77 <ccastillo.perez77@gmail.com>
2021-03-03 20:54:27 -0600

    colocando un input

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
-
+import {AddCategory} from './components/AddCategory'
 export default function GifExpertApp() {
 
     //const categories = ['One pounch','Dragon Ball','Samurai X']
     const [categories,setCategories] = useState(['One pounch','Dragon Ball','Samurai X','Final Fantasy','Mortal Kombat X1'])
 
+    /*
     const addCategory = () =>{
         setCategories([...categories,'Nueva categoria'])
     }
+    */
 
     return (
         <div>
             <h2>Gif Expert App</h2>
+            <AddCategory />
             <hr/>
-            <button onClick={addCategory}>Agregar</button>
+
             <ul>
              {
                  categories.map((cat,i) => {
