337c2449b433ab8d95091be013ab1e9648c69884
SOLRAK77 <ccastillo.perez77@gmail.com>
2021-03-05 10:40:25 -0600

    Se genera componente peticion fetch

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import {AddCategory} from './components/AddCategory'
+import {GifGrid} from './components/GifGrid'
 export default function GifExpertApp() {
 
     //const categories = ['One pounch','Dragon Ball','Samurai X']
-    const [categories,setCategories] = useState(['One pounch','Dragon Ball','Samurai X','Final Fantasy','Mortal Kombat X1'])
+    const [categories,setCategories] = useState(['Mortal Kombat X1'])
 
     /*
     const addCategory = () =>{
@@ -16,12 +17,14 @@ export default function GifExpertApp() {
             <h2>Gif Expert App</h2>
             <AddCategory setCategories={setCategories} />
             <hr/>
-
             <ul>
-             {
-                 categories.map((cat,i) => {
-                     return (<li key={i}>{cat}</li>)
-                 })
+             {                 
+                categories.map((cat,i) => (
+                    <GifGrid 
+                        key={`categoria-${i}`}
+                        category={cat} 
+                    />
+                ))
              }   
             </ul>
         </div>
