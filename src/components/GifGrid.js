c7bdf42246e1446d2fd2d38f5e9cc05cd269585e
SOLRAK77 <ccastillo.perez77@gmail.com>
2021-03-06 02:27:21 -0600

    se agrega fetch API, componente hijo y manejo de CSS

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {GifGridItem} from './GifGridItem'
 
 export const GifGrid = ({category}) => {
 
+    //const [contador, setContador] = useState(0);
+    const [images, setImages] = useState([])
+
+    useEffect(() => {
+        getGifs();
+    },[])
+
     const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick and Monty&limit=10&api_key=x3j3hc9n8Z3kCdjW4nTA8SWe2OnuvAlzf'        
-
         const resp = await fetch(url)
         const{data} = await resp.json()        
+
         const gifs = data.map( img => {
             return {
                 id : img.id,
@@ -15,15 +23,28 @@ export const GifGrid = ({category}) => {
             }
         })
 
-        console.log(gifs)
-
+        setImages(gifs);
     }
 
-    //getGifs();
+    /*
+    const suma = () => {
+        setContador(contador + 1)
+    }
+    */
 
     return (
-        <div>
+        <>
             <h3>{category}</h3>
-        </div>
+            <div className="card--grid">        
+                    {
+                        images.map( (img,i) => {
+                            return (<GifGridItem 
+                                key={`Image${i}`}
+                                {...img} />
+                            )
+                        })
+                    }
+            </div>
+        </>
     )
 }
